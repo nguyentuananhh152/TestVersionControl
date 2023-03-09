fda6c6b1ab52e10559a8bc2fe40510f5c6e89e61
tuananh <nguyentuananhh152@gmail.com>
2023-03-10 00:38:02 +0700

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world test OK!");
+        System.out.println("Hello world test main 2");
     }
 }

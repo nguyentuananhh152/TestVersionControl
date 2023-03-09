ba86d800afbe5bc9d1509a2f271b84cf99064990
tuananh <nguyentuananhh152@gmail.com>
2023-03-10 00:38:07 +0700

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world test clone");
+        System.out.println("Hello world test clone 2");
     }
 }
